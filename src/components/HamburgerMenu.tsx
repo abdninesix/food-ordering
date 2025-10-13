d3af4cc87b1b11a22399990428826195daf17c92
abdninesix <mohabd1995@gmail.com>
2025-10-13 11:45:49 +0500

    Updated

---
+++ src/components/HamburgerMenu.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+        <Image src="/" alt="" width={20} height={20}/>
+    </div>
+  )
+}
+
+export default Menu
