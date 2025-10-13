623ea3f5df3d43507cc2bd0a8329c5ce7fa1870a
abdninesix <mohabd1995@gmail.com>
2025-10-13 23:51:57 +0500

    Updated

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,11 +1,18 @@
 "use client"
 
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CartIcon = () => {
-  return (
-    <div>CartIcon</div>
-  )
+    return (
+        <Link href="/cart" className='flex items-center gap-2'>
+            <div className='relative size-8'>
+                <Image src="/cart.png" alt="CartIcon" fill />
+            </div>
+            <span>Cart (3)</span>
+        </Link>
+    )
 }
 
 export default CartIcon
