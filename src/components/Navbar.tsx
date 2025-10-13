c5bc1e89caebe197c4d2caa157b98203d300fbee
abdninesix <mohabd1995@gmail.com>
2025-10-13 23:59:27 +0500

    Updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,20 @@ import CartIcon from './CartIcon'
 const Navbar = () => {
   return (
     <div className='h-12 text-red-500 p-4 flex items-center justify-between border-b-2 uppercase'>
+      {/* NAV LINKS */}
+      <div className='hidden md:flex gap-4'>
+        <Link href="/"></Link>
+        <Link href="/"></Link>
+        <Link href="/"></Link>
+        <CartIcon />
+      </div>
+
       {/* LOGO */}
-        <Link href="/" className='text-xl'>Yummies</Link>
+      <Link href="/" className='text-xl'>Yummies</Link>
 
       {/* HAMBURGER */}
       <div className='block md:hidden'>
         <HamburgerMenu />
-      </div>
-
-      {/* NAV LINKS */}
-      <div className='hidden md:flex'>
-        <CartIcon/>
       </div>
     </div>
   )
