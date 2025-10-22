4c07cfe9bea577aa4880abc4e27c1d781e3a51a6
abdninesix <mohabd1995@gmail.com>
2025-10-22 22:00:44 +0500

    Updated

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,22 @@
-import { menu } from "@/data";
+// import { menu } from "@/data";
+import { MenuType } from "@/types/types";
 import Link from "next/link";
 import React from "react";
 
-const MenuPage = () => {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+const MenuPage = async () => {
+
+  const menu:MenuType = await getData();
+
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
@@ -12,10 +26,10 @@ const MenuPage = () => {
           className="w-full h-1/3 bg-cover p-8 md:h-1/2"
           style={{ backgroundImage: `url(${category.img})` }}
         >
-          <div className={`text-${category.color} w-1/2`}>
+          <div className={`text-${category.color} w-1/2 space-y-4`}>
+            <button className={`hidden 2xl:block bg-black text-white py-2 px-4 rounded-md`}>Explore</button>
             <h1 className="uppercase font-bold text-3xl">{category.title}</h1>
-            <p className="text-sm my-8">{category.desc}</p>
-            <button className={`hidden 2xl:block bg-${category.color} text-${category.color === "black" ? "white" : "red-500"} py-2 px-4 rounded-md`}>Explore</button>
+            <p className="text-sm">{category.desc}</p>
           </div>
         </Link>
       ))}
