0eeed16a8de1934a5156e1ee2e20a92db9daf566
abdninesix <mohabd1995@gmail.com>
2025-10-23 10:59:13 +0500

    Updated

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -4,9 +4,10 @@ import { NextRequest, NextResponse } from "next/server"
 export const GET = async (req: NextRequest) => {
 
     const { searchParams } = new URL(req.url)
+    const cat = searchParams.get("cat")
 
     try {
-        const products = await prisma.product.findMany();
+        const products = await prisma.product.findMany({ where: { ...(cat ? { catSlug: cat } : { isFeatured: true }) } });
         return new NextResponse(JSON.stringify(products), { status: 200 })
     } catch (error) {
         console.log(error)
