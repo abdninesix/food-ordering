82d585547735ea7a97af50562ac3b1b3c76ca178
abdninesix <mohabd1995@gmail.com>
2025-10-23 16:40:07 +0500

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,14 +20,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="overflow-x-hidden">
-          <Notification />
-          <Navbar />
-          {children}
-          <Footer />
-        </div>
-      </body>
+      <SessionProvider >
+        <body className={inter.className}>
+          <div className="overflow-x-hidden">
+            <Notification />
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </body>
+      </SessionProvider>
     </html>
   );
 }
