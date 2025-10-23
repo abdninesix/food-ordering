e736fae07650b0c5a92697472120da5cd6948d05
abdninesix <mohabd1995@gmail.com>
2025-10-23 19:55:54 +0500

    Updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,9 @@ import React from "react";
 
 const LoginPage = () => {
 
+  const { data: session, status } = useSession();
+  console.log(status, session);
+
   return (
     <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
       {/* BOX */}
@@ -19,7 +22,7 @@ const LoginPage = () => {
         <div className="p-10 flex flex-col gap-8 md:w-1/2">
           <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md" onClick={()=>signIn("google")}>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md" onClick={() => signIn("google")}>
             <Image
               src="/google.png"
               alt=""
