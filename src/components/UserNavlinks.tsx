1e97186df8459ecbdbe27a677ceb7284332771a9
abdninesix <mohabd1995@gmail.com>
2025-10-25 23:20:32 +0500

    Updated

--- src/components/UserNavlinks.tsx
+++ src/components/UserNavlinks.tsx
@@ -1,11 +1,25 @@
 "use client"
 
+import { signOut, useSession } from 'next-auth/react'
+import Link from 'next/link';
 import React from 'react'
 
 const UserNavlinks = () => {
-  return (
-    <div>UserNavlinks</div>
-  )
+
+    const { status } = useSession();
+
+    return (
+        <div>
+            {status === "authenticated" ? (
+                <>
+                    <Link href="/orders">Orders</Link>
+                    <button onClick={() => signOut()}>Logout</button>
+                </>
+            ) : (
+                <Link href="/login">Login</Link>
+            )}
+        </div>
+    )
 }
 
 export default UserNavlinks
