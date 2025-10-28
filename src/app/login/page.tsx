8385007e1cef14e371801ab8d43655c0bbc2f85c
abdninesix <mohabd1995@gmail.com>
2025-10-28 15:56:45 +0500

    Updated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,14 +22,14 @@ const LoginPage = () => {
       {/* BOX */}
       <div className=" h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
         {/* IMAGE CONTAINER */}
-        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+        <div className="relative h-1/3 w-full md:h-full md:w-1/2 overflow-hidden">
           <Image src="/loginBg.png" alt="" width={1024} height={1024} className="object-cover" />
         </div>
         {/* FORM CONTAINER */}
         <div className="p-10 flex flex-col gap-8 md:w-1/2">
           <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
           <p>Log into your account or create a new one using social buttons</p>
-          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md" onClick={() => signIn("google")}>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md cursor-pointer" onClick={() => signIn("google")}>
             <Image
               src="/google.png"
               alt=""
