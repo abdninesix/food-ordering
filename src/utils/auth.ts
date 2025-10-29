88f58f0087bee5a1fba1b8c276e3d86cb2582de1
abdninesix <mohabd1995@gmail.com>
2025-10-29 19:40:59 +0500

    Updated

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,27 +3,7 @@ import NextAuth, { User } from "next-auth"
 import Google from "next-auth/providers/google"
 import prisma from "./connect"
 
-declare module "next-auth" {
-  interface Session {
-    user: User & {
-      isAdmin: boolean
-    }
-  }
-}
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    isAdmin: boolean
-  }
-}
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  session: { strategy: "jwt" },
   adapter: PrismaAdapter(prisma),
   providers: [Google],
-  callbacks: {
-    async session({ token, session }) {
-      if (token) { session.user.isAdmin = token.isAdmin }
-    }
-  }
 })
