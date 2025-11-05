c7fdda28e5ad64d16e7825a94a44aa17744c0365
abdninesix <mohabd1995@gmail.com>
2025-11-05 23:44:44 +0500

    Updated

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -24,7 +24,7 @@ const Price = ({ price, id, options }: Props) => {
       <h2 className="text-2xl font-bold">${total}</h2>
       {/* OPTIONS CONTAINER */}
       <div className="flex gap-4">
-        {options?.map((option, index) => (
+        {options?.length && options?.map((option, index) => (
           <button
             key={option.title}
             className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md"
