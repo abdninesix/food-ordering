98d690eb634007e04b5110f72e1c1d70e5ea98eb
abdninesix <mohabd1995@gmail.com>
2025-11-07 23:20:02 +0500

    Updated

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,3 +1,10 @@
+import { ActionType, CartType } from "@/types/types"
+import { create } from "zustand"
+
 const INITIAL_STATE = {
-    product: []
+    product: [],
+    totalItems: 0,
+    totalPrice: 0,
 }
+
+export const useCartStore = create<CartType & ActionType>(get, set)
