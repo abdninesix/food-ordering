86e49d1a48ed89a47a19b646e6490f65f883c231
abdninesix <mohabd1995@gmail.com>
2025-11-07 23:37:07 +0500

    Updated

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -2,9 +2,29 @@ import { ActionType, CartType } from "@/types/types"
 import { create } from "zustand"
 
 const INITIAL_STATE = {
-    product: [],
+    products: [],
     totalItems: 0,
     totalPrice: 0,
 }
 
-export const useCartStore = create<CartType & ActionType>(get, set)
+export const useCartStore = create<CartType & ActionType>((set, get) => ({
+    products: INITIAL_STATE.products,
+    totalItems: INITIAL_STATE.totalItems,
+    totalPrice: INITIAL_STATE.totalPrice,
+
+    addToCart(item) {
+        set((state) => ({
+            products: [...state.products],
+            totalItems: state.totalItems + item.quantity,
+            totalPrice: state.totalPrice + item.price,
+        }))
+    },
+
+    removeFromCart(item) {
+        set((state) => ({
+            products: state.products.filter((product) => product.id !== item.id),
+            totalItems: state.totalItems - item.quantity,
+            totalPrice: state.totalPrice - item.price,
+        }))
+    }
+}))
