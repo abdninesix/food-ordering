4fee830e9a0debc01c59f6d369cb80d0e4ea7fcb
abdninesix <mohabd1995@gmail.com>
2025-11-08 23:44:08 +0500

    Updated

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -22,7 +22,7 @@ const SingleProductPage = async ({ params }: { params: { id: string } }) => {
     <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center">
       {/* IMAGE CONTAINER */}
       {singleProduct.img && (
-        <div className="relative w-full h-1/2 md:h-[70%]">
+        <div className="relative w-full h-1/2 md:w-1/2 md:h-[70%]">
           <Image
             src={singleProduct.img}
             alt=""
