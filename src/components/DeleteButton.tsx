e325651449f2e5ddac380a4a99db4563681600c6
abdninesix <mohabd1995@gmail.com>
2025-11-10 20:31:16 +0500

    Updated

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -1,11 +1,23 @@
 "use client"
 
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const DeleteButton = () => {
-  return (
-    <button className='bg-red-500 text-white absolute top-10 right-10 p-2'>Delete</button>
-  )
+
+    const { data: session, status } = useSession()
+    const router = useRouter()
+
+    if (status === "loading") return <div className="text-center">Loading orders list...</div>
+
+    if (status === "unauthenticated" || !session?.user.isAdmin) {
+        return;
+    }
+
+    return (
+        <button className='bg-red-500 text-white absolute top-10 right-10 p-2 cursor-pointer'>Delete</button>
+    )
 }
 
 export default DeleteButton
