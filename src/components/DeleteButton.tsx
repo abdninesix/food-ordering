db065fa1371f04067105e033410e5b52898740c8
abdninesix <mohabd1995@gmail.com>
2025-11-10 20:58:02 +0500

    Updated

--- src/components/DeleteButton.tsx
+++ src/components/DeleteButton.tsx
@@ -3,14 +3,22 @@
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import { toast } from 'react-toastify'
 
 const DeleteButton = ({ id }: { id: number }) => {
 
     const { data: session, status } = useSession()
     const router = useRouter()
 
-    const handleDelete = () => {
-
+    const handleDelete = async () => {
+        const res = await fetch(`http://localhost:3000/api/products/${id}`, { method: "DELETE" })
+        if (res.status === 200) {
+            router.push("/menu")
+            toast.success("Product deleted!")
+        } else {
+            const data = await res.json()
+            toast.error(data.message)
+        }
     }
 
     if (status === "loading") return <div className="text-center">Loading orders list...</div>
