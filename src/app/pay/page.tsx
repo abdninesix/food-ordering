e10811d28eeb6f6889fdba13ae47cc25bacbaf95
abdninesix <mohabd1995@gmail.com>
2025-11-11 23:03:03 +0500

    Updated

--- src/app/pay/page.tsx
+++ src/app/pay/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const PaymentPage = () => {
   return (
-    <div>page</div>
+    <div>PaymentPage</div>
   )
 }
 
-export default page
+export default PaymentPage
