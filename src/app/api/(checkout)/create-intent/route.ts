2e69e5d360bf9b50a706234865dc66598b8a6200
abdninesix <mohabd1995@gmail.com>
2025-11-22 14:17:58 +0500

    Updated

--- src/app/api/(checkout)/create-intent/route.ts
+++ src/app/api/(checkout)/create-intent/route.ts
@@ -1,5 +1,8 @@
+import prisma from "@/utils/connect";
+
 const stripe = require("stripe")(process.env.STRIPE_SECTRET_KEY)
 
 export const POST = async ({ params }: { params: { id: string } }) => {
     const { id } = params;
+    const order = await prisma.orders.findUnique({ where: { id: id } });
 }
