4591d9e7e27c5ceea5d6f2f7d4ba46c7eae28d7d
abdninesix <mohabd1995@gmail.com>
2025-11-22 15:06:54 +0500

    Updated

--- src/app/api/(checkout)/create-intent/route.ts
+++ src/app/api/(checkout)/create-intent/route.ts
@@ -8,7 +8,15 @@ export const POST = async ({ params }: { params: { id: string } }) => {
     const order = await prisma.orders.findUnique({ where: { id: id } });
 
     if (order) {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: 100 * 100,
+            currency: "pkr",
+            automatic_payment_methods: { enabled: true },
+        });
 
+        await prisma.orders.update({ where: { id:id }, data: { intent_id: paymentIntent.id } });
+
+        return new NextResponse(JSON.stringify({ clientSecret: paymentIntent.client_secret }), { status: 200 })
     } else {
         return new NextResponse(JSON.stringify({ message: "Something went wrong!" }), { status: 404 })
     }
