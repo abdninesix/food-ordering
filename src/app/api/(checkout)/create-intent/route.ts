86d927d80cf692e3b8fe9adb8d5dd9c0128696a0
abdninesix <mohabd1995@gmail.com>
2025-11-22 14:37:32 +0500

    Updated

--- src/app/api/(checkout)/create-intent/route.ts
+++ src/app/api/(checkout)/create-intent/route.ts
@@ -1,8 +1,15 @@
 import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
 
 const stripe = require("stripe")(process.env.STRIPE_SECTRET_KEY)
 
 export const POST = async ({ params }: { params: { id: string } }) => {
     const { id } = params;
     const order = await prisma.orders.findUnique({ where: { id: id } });
+
+    if (order) {
+
+    } else {
+        return new NextResponse(JSON.stringify({ message: "Something went wrong!" }), { status: 404 })
+    }
 }
