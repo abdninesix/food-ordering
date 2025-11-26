428f7674ba13c9fa9ca73e7e6147525c6c36c10f
abdninesix <mohabd1995@gmail.com>
2025-11-26 18:34:50 +0500

    Updated

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,10 +1,28 @@
 "use client"
 
-import React from 'react'
+import { useRouter, useSearchParams } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 const SucessPage = () => {
+
+    const searchParams = useSearchParams()
+    const payment_intent = searchParams.get("payment_intent")
+    const router = useRouter()
+
+    useEffect(() => {
+        const makeRequest = async () => {
+            try {
+                await fetch(`http://localhost:3000/api/confirm/${payment_intent}`, { method: "PUT" })
+                router.push("/orders")
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        makeRequest();
+    }, [payment_intent, router])
+
     return (
-        <div>SucessPage</div>
+        <div className="mt-10 text-center">Payment succesfull. Please do not close the page. You are being redirected to the orders page.</div>
     )
 }
 
