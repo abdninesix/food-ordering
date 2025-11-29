b4e212ae6dd7db9c94d8454cb928e7e449ac4da9
abdninesix <mohabd1995@gmail.com>
2025-11-29 18:17:22 +0500

    Updated

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const AddPage = () => {
+    return (
+        <div>
+            <form>
+
+            </form>
+        </div>
+    )
 }
 
-export default page
+export default AddPage
