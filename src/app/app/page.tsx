25ed1cf9808b389eb97d2184a90c33809279f725
abdninesix <mohabd1995@gmail.com>
2025-11-29 18:19:27 +0500

    Updated

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -6,7 +6,23 @@ const AddPage = () => {
     return (
         <div>
             <form>
-
+                <h1>Add New Product</h1>
+                <div>
+                    <label></label>
+                    <input type="" />
+                </div>
+                <div>
+                    <label></label>
+                    <input type="" />
+                </div>
+                <div>
+                    <label></label>
+                    <input type="" />
+                </div>
+                <div>
+                    <label></label>
+                    <input type="" />
+                </div>
             </form>
         </div>
     )
