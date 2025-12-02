305ea26ff9f19c6604bbdac5724b9915b27eb4c6
abdninesix <mohabd1995@gmail.com>
2025-12-02 19:46:45 +0500

    Updated

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -16,14 +16,10 @@ export const GET = async (req: NextRequest) => {
 }
 
 export const POST = async (req: NextRequest) => {
-
-    const body = await req.json()
-    const { searchParams } = new URL(req.url)
-    const cat = searchParams.get("cat")
-
     try {
-        const products = await prisma.product.findMany({ where: { ...(cat ? { catSlug: cat } : { isFeatured: true }) } });
-        return new NextResponse(JSON.stringify(products), { status: 200 })
+        const body = await req.json()
+        const product = await prisma.product.create({ data: body });
+        return new NextResponse(JSON.stringify(product), { status: 200 })
     } catch (error) {
         console.log(error)
         return new NextResponse(JSON.stringify({ message: "Something went wrong" }), { status: 500 })
