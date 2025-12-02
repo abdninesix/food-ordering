7361ad2eebf9071fbfac72a5b77df5e43e5fcb88
abdninesix <mohabd1995@gmail.com>
2025-12-02 19:45:19 +0500

    Updated

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -14,3 +14,18 @@ export const GET = async (req: NextRequest) => {
         return new NextResponse(JSON.stringify({ message: "Something went wrong" }), { status: 500 })
     }
 }
+
+export const POST = async (req: NextRequest) => {
+
+    const body = await req.json()
+    const { searchParams } = new URL(req.url)
+    const cat = searchParams.get("cat")
+
+    try {
+        const products = await prisma.product.findMany({ where: { ...(cat ? { catSlug: cat } : { isFeatured: true }) } });
+        return new NextResponse(JSON.stringify(products), { status: 200 })
+    } catch (error) {
+        console.log(error)
+        return new NextResponse(JSON.stringify({ message: "Something went wrong" }), { status: 500 })
+    }
+}
