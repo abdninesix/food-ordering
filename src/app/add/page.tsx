c09864165aef84845b3506faecde8e83bfedcf79
abdninesix <mohabd1995@gmail.com>
2025-12-03 23:28:53 +0500

    Updated

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -65,7 +65,7 @@ const AddPage = () => {
         const data = new FormData()
         data.append("file", file!)
         data.append("upload_preset", "food-ordering")
-        const res = await fetch("https://api.cloudinary.com/v1_1/:cloud_name/image", {
+        const res = await fetch("https://api.cloudinary.com/v1_1/dvjbkun4a/image", {
             method: "POST",
             body: data,
         })
